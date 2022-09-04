117489b9186ee44170313b30059ea964c2de8b21
Yotam Granada <yotam.granada@gmail.com>
2022-09-04 13:54:25 +0300

    upload begining files

---
+++ app.js
@@ -0,0 +1,38 @@
+// DO NOT DELETE ANY OF THE EXISTING CODE IN THIS FILE
+
+//Change the text color of all nav links to red
+
+  
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM has been fully loaded");
+  console.table(animals);
+
+  const navItemes = document.querySelectorAll(".nav__item");
+  console.log(navItemes)
+  
+
+navItemes.forEach((nav_item) => {
+    nav_item.style
+});
+  //Your code here
+  //Select the cards container div by its class
+const cardsContainer = document.querySelector(".cards-container");
+
+  //Iterate over the array of animal data and for each animal, use DOM manipulation to create a card that meets the following requirements:
+// cardsContainer.forEach
+
+  
+
+  //The card's contents are contained by a parent div element with a class attribute of card as well as a style attribute that sets the card's width to 18rem
+
+  //Card has an image element with attributes, including a class of card-img-top, src of the animal's image url
+
+  //Card has a child div with a class of card-body
+
+  //In the card's body, there is a heading element with a class of card-title and the animal's name as text
+
+  //Below the card title, there is a paragraph element that displays the animal's description
+
+})
---
+++ data.js
@@ -0,0 +1,27 @@
+const animals = [
+    {
+        id: 1,
+        name: 'dog',
+        image: 'https://images.unsplash.com/photo-1548199973-03cce0bbc87b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+        description: 'Doggo ipsum shoob blep noodle horse borking doggo pats, smol borking doggo with a long snoot for pats very hand that feed shibe bork, maximum borkdrive what a nice floof long woofer. Boofers clouds many pats porgo borkf, boofers fat boi mlem, heckin good boys fluffer heckin good boys and girls. Wow such tempt length boy heckin good boys very jealous pupper thicc what a nice floof, fluffer shoob super chub. Borkf boofers stop it fren the neighborhood pupper wrinkler, shoob fat boi. '
+    },
+    {
+        id: 2,
+        name: 'cat',
+        image:
+            'https://images.unsplash.com/photo-1532386236358-a33d8a9434e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        description: 'Loves cheeseburgers. If it fits, i sits chase after silly colored fish toys around the house. Gate keepers of hell munch on tasty moths yet suddenly go on wild-eyed crazy rampage bite the bratty kid floof tum, tickle bum, jellybean footies curly toes for sniff other cat butt and hang jaw half open thereafter. '
+    },
+    {
+        id: 3,
+        name: "monkey",
+        image: 'https://images.unsplash.com/photo-1430462773665-fd261133b47f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1955&q=80',
+        description: 'Tree banana rainforest poo spider arboreal endangered capuchin zoo space monkey monkey primate new world howler baby exotic. Tree rainforest monkey endangered new world poo space monkey baby exotic capuchin zoo howler primate banana arboreal spider. '
+    },
+    {
+        id: 4,
+        name: 'moose',
+        image: 'https://images.unsplash.com/photo-1534804442465-9689d85be37b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1949&q=80',
+        description: 'Lorizzle ipsum bling bling sit amizzle, consectetuer adipiscing elit. Nizzle sapien velizzle, bling bling volutpat, suscipit , gravida vel, arcu. Check it out hizzle that\'s the shizzle.We gonna chung erizzle.Fo izzle dolor fo turpis tempizzle tempor.Gangsta boom shackalack mofo et turpizzle.Sizzle izzle tortor.Pellentesque uhuh'
+    }
+]
